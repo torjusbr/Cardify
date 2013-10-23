2c2007b0ac10e015503faeed52dd241d83d44ee5
retterst <retterst@gouter.eurecom.fr>
2013-10-23 15:28:55 +0200

    conflict

--- gen/fr/eurecom/cardify/R.java
+++ gen/fr/eurecom/cardify/R.java
@@ -24,9 +24,11 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
+        public static final int action_settings=0x7f080004;
         public static final int button1=0x7f080001;
+        public static final int lobbyView=0x7f080002;
         public static final int textView1=0x7f080000;
+        public static final int textView2=0x7f080003;
     }
     public static final class layout {
         public static final int activity_game=0x7f030000;
@@ -44,6 +46,7 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int lobby_game=0x7f050006;
         public static final int title_activity_game=0x7f050004;
         public static final int title_activity_host_game=0x7f050003;
         public static final int title_activity_lobby=0x7f050005;
