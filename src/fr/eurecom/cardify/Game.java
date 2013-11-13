fba068462130dfb1e6316e5dc290bc3236568a14
Christian Holm Hansen <christian.ahh@gmail.com>
2013-11-13 12:49:08 +0100

    Changed card bitmaps' names and flæflæ move cards around

--- src/fr/eurecom/cardify/Game.java
+++ src/fr/eurecom/cardify/Game.java
@@ -1,26 +1,121 @@
 package fr.eurecom.cardify;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
 import android.app.Activity;
-import android.util.Log;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
+import android.widget.HorizontalScrollView;
+import android.widget.RelativeLayout;
+import fr.eurecom.util.Card;
 
 public class Game extends Activity {
 
+	private ArrayList<Card> playerCards;
+	private Point screenSize;
+	private HorizontalScrollView scrollView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_game);
+		
+		Display display = getWindowManager().getDefaultDisplay();
+		screenSize = new Point();
+		display.getSize(screenSize);
+
+		
+		testCards();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.game, menu);
-		Log.d("bl¾", "e");
 		return true;
 	}
 	
+	private void testCards() {
+		playerCards = new ArrayList<Card>();
+		playerCards.add(new Card(this,'h',5));
+		playerCards.add(new Card(this,'h',6));
+		playerCards.add(new Card(this,'h',7));
+		playerCards.add(new Card(this,'h',8));
+		playerCards.add(new Card(this,'h',9));
+		playerCards.add(new Card(this,'h',10));
+		playerCards.add(new Card(this,'h',11));
+		playerCards.add(new Card(this,'h',12));
+		playerCards.add(new Card(this,'h',13));
+		playerCards.add(new Card(this,'s',1));
+		playerCards.add(new Card(this,'h',4));
+		playerCards.add(new Card(this,'h',3));
+		playerCards.add(new Card(this,'h',1));
+		playerCards.add(new Card(this,'c',1));
+		playerCards.add(new Card(this,'h',2));
+		playerCards.add(new Card(this,'d',1));
+		
+		Collections.sort(playerCards, new CardComporator(SortingMethod.S_H_D_C_ACE_HIGH));
+		
+		drawCards();
+	}
+	
+	private void drawCards() {
+		RelativeLayout layout = (RelativeLayout) findViewById(R.id.rootGameLayout);
+		
+		final int cardSpace = screenSize.x/playerCards.size() - (Card.cardWidth-screenSize.x/playerCards.size())/playerCards.size();
+		
+		int yPos = screenSize.y-Card.cardHeight;
+		for(int i = 0; i < playerCards.size(); i++) {
+			int xPos = i*cardSpace;
+			Card c = playerCards.get(i);
+			c.setX(xPos);
+			c.setY(yPos);
+			layout.addView(c);
+		}
+	}
+	
+	private enum SortingMethod {
+		S_H_D_C_ACE_HIGH,
+		NO_SUIT_ACE_HIGH;
+	}
+	
+	private final class CardComporator implements Comparator<Card> {
+		private ArrayList<Character> suitArray = new ArrayList<Character>();
+		private SortingMethod sorting;
 
-
+		public CardComporator(SortingMethod sorting) {
+			this.sorting = sorting;
+			
+			switch (sorting) {
+				case S_H_D_C_ACE_HIGH:
+					suitArray.add('s');
+					suitArray.add('h');
+					suitArray.add('d');
+					suitArray.add('c');
+					break;
+			}
+		}
+		
+		@Override
+		public int compare(Card a, Card b) {
+			
+			switch (sorting) {
+				case S_H_D_C_ACE_HIGH:
+					if(a.getSuit() == b.getSuit()) {
+						return a.getFace()-b.getFace();
+					} else {
+						return suitArray.indexOf(b.getSuit())-suitArray.indexOf(a.getSuit());
+					}
+				default:
+					return 0;
+			}
+		}
+		
+		
+		
+	}
 }
--- src/fr/eurecom/util/Card.java
+++ src/fr/eurecom/util/Card.java
@@ -1,13 +1,123 @@
 package fr.eurecom.util;
 
 import android.content.Context;
-import android.view.SurfaceView;
+import android.graphics.Point;
+import android.util.Log;
+import android.view.Display;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup.LayoutParams;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
-public class Card extends SurfaceView {
+public class Card extends ImageView implements OnTouchListener {
 
+	public static int cardHeight = 208;
+	public static int cardWidth = 150;
+	
+	
+	private char suit;
+	private int face;
+	private int imageResource;
+	private Point anchorPoint = new Point();
+	private Point screenSize;
+	
 	public Card(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
+	}
+	
+	public Card(Context context, char suit, int face) {
+		super(context);
+		this.suit = suit;
+		this.face = face;
+		
+		String resourceString = "drawable/"+suit+face;
+		imageResource = getImageResource(context, resourceString);
+		
+		this.setImageResource(imageResource);
+		this.setLayoutParams(new LayoutParams(cardWidth, cardHeight));
+		
+		this.setOnTouchListener(this);
+		
+		WindowManager wm = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+		Display display = wm.getDefaultDisplay();
+		screenSize = new Point();
+		display.getSize(screenSize);
+	}
+	
+	private static int getImageResource(Context context, String string) {		
+		return context.getResources().getIdentifier(string, null, context.getPackageName());
 	}
 
+	@Override
+	public boolean onTouch(View v, MotionEvent event) {
+		final int action = event.getAction();
+		
+		switch(action) {
+			case MotionEvent.ACTION_DOWN:
+				
+				Log.e("TOUCH","ACTION_DOWN");
+				
+				//Calculate the anchor point (x,y) in card image
+				anchorPoint.x = (int) (event.getRawX() - v.getX());
+				anchorPoint.y = (int) (event.getRawY() - v.getY());
+				
+				//TODO: Add better dragging graphics
+				v.setAlpha((float)0.5);
+				v.bringToFront();
+		    	break;
+		    	
+			case MotionEvent.ACTION_MOVE:
+				
+				Log.e("TOUCH","ACTION_MOVE");
+				
+                int x = (int) event.getRawX();
+                int y = (int) event.getRawY();
+                
+                v.setX(x-(anchorPoint.x));
+                v.setY(y-(anchorPoint.y));
+                
+				break;
+				
+			case MotionEvent.ACTION_UP:
+				Log.e("TOUCH","ACTION_UP");
+				Log.e("DROP_POSITION","("+v.getX()+","+v.getY()+")");
+				
+				//ScaleAnimation animation = new ScaleAnimation(v.getX(), v.getX()+50, v.getY(), v.getY()+50);
+				//TranslateAnimation animation = new TranslateAnimation(0, 100, 0, 100);
+				//animation.setDuration(1000);
+				//startAnimation(animation);
+				
+				if(v.getX() < Card.cardWidth/2) {
+					if(v.getY() < Card.cardHeight/2) {
+						TranslateAnimation animation = new TranslateAnimation(0, Math.abs(v.getX()), 0, Math.abs(v.getY()));
+						animation.setDuration(500);
+						startAnimation(animation);
+						v.setX(0);
+						v.setY(0);
+					}
+				}
+				
+				v.setAlpha(1);
+				break;
+
+			default:
+				Log.e("TOUCH", "OTHER TOUCH EVENT NO."+action);
+				break;
+		}
+		return true;
+	}
+
+	public char getSuit() {
+		return suit;
+	}
+
+	public int getFace() {
+		return face;
+	}
 }
+
