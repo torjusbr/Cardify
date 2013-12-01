bb00e2037371d27430c01ad2b8072c859477a701
Torjus Bryne Retterstøl <torjus.br@gmail.com>
2013-12-01 20:16:53 +0100

    ny port

--- src/fr/eurecom/messaging/Config.java
+++ src/fr/eurecom/messaging/Config.java
@@ -3,6 +3,6 @@ package fr.eurecom.messaging;
 public class Config {
 
 	
-	public final static int PORT = 8888;
+	public final static int PORT = 4321;
 	public final static int TIMEOUT_INTERVAL = 500;
 }
