910fb7f42c75deabaaea672264593fe61cb29a35
Martin Kirkholt Melhus <martin.melhus@gmail.com>
2013-12-01 13:54:27 +0100

    Changed commit-bug in HostGame, sync is now safe!

--- src/fr/eurecom/cardify/HostGame.java
+++ src/fr/eurecom/cardify/HostGame.java
@@ -11,7 +11,7 @@ public class HostGame extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
+		setContentView(R.layout.activity_host_game);
 	}
 
 	@Override
