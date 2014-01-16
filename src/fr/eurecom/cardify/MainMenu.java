207f6a483f459c6536cf9989e5533dacf018924a
Torjus Bryne Retterstøl <torjus.br@gmail.com>
2014-01-16 22:54:17 +0100

    Exits app when back pressed main menu

--- src/fr/eurecom/cardify/MainMenu.java
+++ src/fr/eurecom/cardify/MainMenu.java
@@ -8,34 +8,41 @@ import android.view.View;
 
 public class MainMenu extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_menu);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return true;
-    }
-    
-    public void joinGame(View view) {
-    	
-    }
-    
-    public void lobby(View view) {
-    	Intent intent = new Intent(this,Lobby.class);
-    	startActivity(intent);
-    }
-    
-    public void startSolitaire(View view) {
+	public void joinGame(View view) {
+
+	}
+
+	public void lobby(View view) {
+		Intent intent = new Intent(this, Lobby.class);
+		startActivity(intent);
+	}
+
+	public void startSolitaire(View view) {
 		Intent intent = new Intent(this, Game.class);
 		intent.putExtra("isSolitaire", true);
 		this.startActivity(intent);
 	}
-    
-    
+
+	@Override
+	public void onBackPressed() {
+//		Should always exit app when back is pressed in main menu
+		Intent intent = new Intent(Intent.ACTION_MAIN);
+		intent.addCategory(Intent.CATEGORY_HOME);
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		startActivity(intent);
+	}
+
 }
